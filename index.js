7f1d3b72ec59f74fbd24f5f4248bc30e2acf878a
Aditya Prajapati <99543597+Adityaprajapati26@users.noreply.github.com>
2022-07-28 12:37:41 +0530

    File uplod

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-function sum(...args){
-    if(arguments.length===0)
-    {
-        return "sum invoked without numbers"
-    }
-    else if(arguments.length==1)
-    {
-        return "sum is not defined"
-    }
-    
-        const sumVal=[...args].reduce((a,c)=>a+Number(c),0)   
-         return Number(sumVal.toFixed(1))
+// const {sum,mul,div,sin,tan} =require("./test")
+// console.log(sum(4,2))
+// console.log(mul(5,8))
+// console.log(div(8,8))
+// console.log(sin(5,8))
+// console.log(tan(55,48))
+// const readline= require('readline')
+// const reader=readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+
+// })
+ 
+
+const prompt = require('prompt-sync')({sigint: true});
+
+
+const numberToGuess = Math.floor(Math.random() * 10) + 1;
+
+let foundCorrectNumber = false;
+
+while (!foundCorrectNumber) {
+ 
+  let guess = prompt('Guess a number from 1 to 10: ');
+
+  guess = Number(guess);
+
+  
+  if (guess === numberToGuess) {
+    console.log('Congrats, you got it!');
+    foundCorrectNumber = true;
+  } else {
+    console.log('Sorry, guess again!');
+  }
 }
-module.exports=sum
+
+// const fs=require("fs")
+// const create=fs.appendFile("text.txt",'data',(err)=>{
+//     console.log("done")
+//    })
+// const data=fs.createWriteStream("text.txt")
+// data.write('hello world')
+// const result=fs.readFileSync("./text.txt",{encoding:"utf-8"})
+// console.log(result)
+// const rename=fs.rename("text.txt","new.txt",(err)=>{
+//     console.log("Rename complete")
+// })
+// const d =fs.unlink("./new.txt",(err)=>{
+//     console.log("delete")
+// })
+
