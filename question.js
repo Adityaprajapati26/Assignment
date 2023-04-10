f644f8b43808f611541bed63fea44c89128a98be
Aditya Prajapati <99543597+Adityaprajapati26@users.noreply.github.com>
2023-04-10 12:56:46 +0530

    Debug Questions

---
+++ question.js
@@ -0,0 +1,95 @@
+
+// Answer 1
+let launch = false;
+let Level = 17000;
+
+if (Level >= 20000) {
+   console.log('Fuel level cleared.');
+   launch = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launch = false;
+}
+// Answer 2
+let launch = false;
+let crew = true;
+let computer = 'green';
+
+if (crew && computer === 'green') {
+   console.log('Crew & computer cleared.');
+   launch = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launch = false;
+}
+
+if (launch) {
+   console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+   console.log("Fed parrot...");
+   console.log("Ignition...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+
+// Answer 3
+let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+// Answer 4
+let launchReady = false;
+let fuelLevel = 27000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log("10, 9, 8...");
+   console.log("Fed parrot...");
+   console.log("6, 5, 4...");
+   console.log("Ignition...");
+   consoul.log("3, 2, 1...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+// Answer 5
+let launchReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
